8f5d4ddfd1f7ee80ad19dbb466653ec3951c1bcb
Alexander Mikasch <13573847+Freakrider@users.noreply.github.com>
2023-09-18 13:27:56 +0200

    Fix locallib.php

--- locallib.php
+++ locallib.php
@@ -18,7 +18,16 @@ function block_contactperson_get_url_of_placeholderimage(): string {
     $fs = get_file_storage();
     $systemcontext = context_system::instance();
     $files = $fs->get_area_files($systemcontext->id, 'block_contactperson', 'placeholderimage', false, 'itemid', false);
+    
+    if (empty($files)) {
+        return '';
+    }
     $file = reset($files);
+    
+    if(empty($file)) {
+        return '';
+    }
+
     $url = moodle_url::make_pluginfile_url($file->get_contextid(), $file->get_component(), $file->get_filearea(),
     $file->get_itemid(), $file->get_filepath(), $file->get_filename());
 
